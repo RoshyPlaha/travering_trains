6e98cae1cca4ceb8856cb5d6a38b336aa2fc2408
Roshy <roshy@roshys-air.home>
2021-04-05 15:55:11 +0100

    added ability to run multiple train files. Separated code into functions

--- graph.py
+++ graph.py
@@ -3,6 +3,8 @@ import networkx as nx
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import figure, text
 import json
+from os import listdir
+from os.path import isfile, join
 
 from dataclasses import dataclass
 
@@ -44,9 +46,12 @@ def load_panel_data(routes):
         routes.add(Route(row['OriginSignal'], row['Exit'], row['TipLocOriginate'], row['TraversalTimes']))
     return routes
 
+def weight_calculator(u, v, d):
+    return d['traversalTime']
+
 def calculate_shortest_path(entry_signal, exit_signal):
     try:
-        return nx.shortest_path(G, entry_signal, exit_signal, weight='traversalTime') # change weight to a function. Going to need make G everytime and and weight to signals in TIPLOCS so it passes through it.
+        return nx.shortest_path(G, entry_signal, exit_signal, weight=weight_calculator) # change weight to a function. Going to need make G everytime and and weight to signals in TIPLOCS so it passes through it.
     except Exception as e:
         print("Could not make a path from signals: ", e)
         return 0
@@ -63,68 +68,99 @@ def find_previous_signal_to_calculate_shortest_path(result, entry_signal, exit_s
         result = calculate_shortest_path(entry_signal, next_signal)
     return result
 
-routes = set([])
-routes = load_panel_data(routes)
 
-G = nx.DiGraph()
-for route in routes:
-    G.add_edge(route.entrySignal, route.exitSignal, isATiploc=route.tiploc, traversalTime=route.traversalTime)
+def setup_infrastructure(graph):
+    routes = set([])
+    routes = load_panel_data(routes)
+
+    for route in routes:
+        graph.add_edge(route.entrySignal, route.exitSignal, isATiploc=route.tiploc, traversalTime=route.traversalTime)
 
 
 def load_trains_journey(filename):
-    with open('train1_journey.json') as f:
+    with open('trains_dir/train_journey_1.json') as f:
       loaded = json.load(f)
       tiplocs = []
       for i in loaded['tiplocs']:
           tiplocs.append(Tiploc(i['station'], i['signal'], i['arrival_time'], i['departure_time']))
     return Journey(loaded['headcode'], tiplocs)
 
-
-train = load_trains_journey('train1_journey.json')
-entry_signal = train.tiplocs[0].signal
-exit_signal = train.tiplocs[-1].signal
-
-# calculate paths from TipLoc to TipLoc in journey - as opposed to just whole journey
-path = []
-for i in range(len(train.tiplocs)-1):
-    entrance, exit = train.tiplocs[i].signal, train.tiplocs[i+1].signal
-    print('Signals to be used: ', (entry_signal, exit_signal))
-    result_to_next_tiploc = calculate_shortest_path(entrance, exit)
-    result = find_previous_signal_to_calculate_shortest_path(result_to_next_tiploc, entry_signal, exit_signal)
-    path.append(result)
-print(' > > ', result) # ['S444', 'S434', 'S430', 'S422', 'S370X', 'S350X', 'S340', 'S306', 'S294', 'S292', 'S288', 'S284', 'S276', 'S270', 'S266', 'S262', 'S252', 'S240', 'S230', 'S224']
-
-def weight_calculator(u, v, d):
-    return d['traversalTime']
-
-other_result = nx.shortest_path(G, 'S333', 'S439', weight=weight_calculator)
-print(other_result)
+def load_train_file_names(path_to_trains_dir):
+    onlyfiles = [f for f in listdir(path_to_trains_dir) if isfile(join(path_to_trains_dir, f))]
+    return onlyfiles
 
 
-red_shortest_distance_edges = []
-for i in range(len(result)-1):
-    try:
-        red_shortest_distance_edges.append((result[i], result[i+1]))
-    except:
-        print('index failure')
+def generate_path(loaded_train):
+    # calculate paths from TipLoc to TipLoc in journey - as opposed to just whole journey
+    paths = []
+    flattened_routes = []
+    for i in range(len(loaded_train.tiplocs) - 1):
+        entrance_signal, exit_signal = loaded_train.tiplocs[i].signal, loaded_train.tiplocs[i + 1].signal
+        entrance_tiploc, exit_tiploc = loaded_train.tiplocs[i].station, loaded_train.tiplocs[i + 1].station
+        print('Signals to be used that map to TipLocs %s - %s are %s - %s: ' %(entrance_tiploc, exit_tiploc, entrance_signal, exit_signal))
+        result_to_next_tiploc = calculate_shortest_path(entrance_signal, exit_signal)
+        print('Routes found between signals are: ', result_to_next_tiploc)
+        result = find_previous_signal_to_calculate_shortest_path(result_to_next_tiploc, entrance_signal, exit_signal)
+        for r in result:
+            if r not in flattened_routes:
+                flattened_routes.append(r)
+        path = {
+            'TipLocs':
+                {
+                    'start_tiploc_name': train.tiplocs[i].station,
+                    'end_tiploc_name': train.tiplocs[i + 1].station,
+                    'preferred_path': result,
+                    'alternative_path': [],
+                }
+        }
+        paths.append(path)
 
-# Specify the edges you want here
-edge_colours = ['black' if not edge in red_shortest_distance_edges else 'red'
-                for edge in G.edges()]
-black_edges = [edge for edge in G.edges() if edge not in red_shortest_distance_edges]
+    return paths, flattened_routes
 
-# Need to create a layout when doing
-# separate calls to draw nodes and edges
-pos = nx.spring_layout(G)
-nx.draw_networkx_nodes(G, pos, cmap=plt.get_cmap('jet'), 
-                       node_color='yellow', node_size=25)
 
-for node, (x, y) in pos.items(): # this is setting the size of the node font
-    text(x, y, node, fontsize=5, ha='center', va='center')
+G = nx.DiGraph()
+setup_infrastructure(graph=G)# in the future, based off what infrastructure is available, we can turn on / off nodes
 
-# nx.draw_networkx_labels(G, pos)
-nx.draw_networkx_edges(G, pos, edgelist=black_edges, arrows=True)
-nx.draw_networkx_edges(G, pos, edgelist=red_shortest_distance_edges, edge_color='r', arrows=True)
+of = load_train_file_names('trains_dir')
+print('There are this many file names to generate paths for: ', len(of))
+
+for filename in of:
+    train = load_trains_journey(filename)
+    print('> > > >  New train %s goes from / to %s, which are Tiplocs from / to %s' %(train.headcode, (train.tiplocs[0].signal, train.tiplocs[-1].signal), (train.tiplocs[0].station, train.tiplocs[-1].station)))
+    tiploc_paths, all_routes = generate_path(train)
+
+    print('< < < < Finished pathing for train ', train.headcode)
+
+    red_shortest_distance_edges = []
+    for i in range(len(all_routes) - 1):
+        try:
+            red_shortest_distance_edges.append((all_routes[i], all_routes[i + 1]))
+        except:
+            print('index failure')
+
+    # Specify the edges you want here
+    edge_colours = ['black' if not edge in red_shortest_distance_edges else 'red'
+                    for edge in G.edges()]
+    black_edges = [edge for edge in G.edges() if edge not in red_shortest_distance_edges]
+
+    # Need to create a layout when doing
+    # separate calls to draw nodes and edges
+    pos = nx.spring_layout(G)
+    nx.draw_networkx_nodes(G, pos, cmap=plt.get_cmap('jet'),
+                           node_color='yellow', node_size=25)
+
+    for node, (x, y) in pos.items():  # this is setting the size of the node font
+        text(x, y, node, fontsize=5, ha='center', va='center')
+
+    # nx.draw_networkx_labels(G, pos)
+    nx.draw_networkx_edges(G, pos, edgelist=black_edges, arrows=True)
+    nx.draw_networkx_edges(G, pos, edgelist=red_shortest_distance_edges, edge_color='r', arrows=True)
+
+    plt.show()
+
+# other_result = nx.shortest_path(G, 'S333', 'S439', weight=weight_calculator)
+# print('ignore ', other_result)
+
 
 # label drawing
 # edge_label_time = nx.get_edge_attributes(G, 'traversalTime') # key is edge, pls check for your case
@@ -135,4 +171,3 @@ nx.draw_networkx_edges(G, pos, edgelist=red_shortest_distance_edges, edge_color=
 # formatted_edge_labels = {(elem[0], elem[1]):edge_label_tippy[elem] for elem in edge_label_tippy} # use this to modify the tuple keyed dict if it has > 2 elements, else ignore
 # nx.draw_networkx_edge_labels(G, pos, edge_labels=formatted_edge_labels, font_color='red')
 
-plt.show()
